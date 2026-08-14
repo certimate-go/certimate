fce5547cb5476e0ff71a0fb882d301c9e609a5fb
Fu Diwei <fudiwei@sina.com>
2026-08-14 10:20:04 +0800

    test: move all providers test to separate packages

---
+++ pkg/core/certmgr/providers-tester/ctcccloud-ao/ctcccloud_ao_test.go
@@ -0,0 +1,48 @@
+package ctcccloudao_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	tester "github.com/certimate-go/certimate/pkg/core/certmgr/providers-tester"
+	impl "github.com/certimate-go/certimate/pkg/core/certmgr/providers/ctcccloud-ao"
+)
+
+var (
+	fp               = tester.InitArgs("CTCCCLOUDAO_")
+	fTestCertPath    string
+	fTestKeyPath     string
+	fAccessKeyId     string
+	fSecretAccessKey string
+)
+
+func init() {
+	fp.DefineString(&fTestCertPath, "TESTCERTPATH")
+	fp.DefineString(&fTestKeyPath, "TESTKEYPATH")
+	fp.DefineString(&fAccessKeyId, "ACCESSKEYID")
+	fp.DefineString(&fSecretAccessKey, "SECRETACCESSKEY")
+}
+
+/*
+Shell command to run this test:
+
+	go test -v ./ctcccloud_ao_test.go -args \
+	--CTCCCLOUDAO_TESTCERTPATH="/path/to/your-test-cert.pem" \
+	--CTCCCLOUDAO_TESTKEYPATH="/path/to/your-test-key.pem" \
+	--CTCCCLOUDAO_ACCESSKEYID="your-access-key-id" \
+	--CTCCCLOUDAO_SECRETACCESSKEY="your-secret-access-key"
+*/
+func TestProvider(t *testing.T) {
+	fp.Parse()
+
+	t.Run("Upload", func(t *testing.T) {
+		provider, err := impl.NewCertmgr(&impl.CertmgrConfig{
+			AccessKeyId:     fAccessKeyId,
+			SecretAccessKey: fSecretAccessKey,
+		})
+		require.NoError(t, err)
+
+		tester.Upload(t, provider, tester.UploadInput{CertPath: fTestCertPath, KeyPath: fTestKeyPath})
+	})
+}
---
+++ pkg/core/certmgr/providers-tester/ksyun-kcm/ksyun_kcm_test.go
@@ -0,0 +1,48 @@
+package ksyunkcm_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	tester "github.com/certimate-go/certimate/pkg/core/certmgr/providers-tester"
+	impl "github.com/certimate-go/certimate/pkg/core/certmgr/providers/ksyun-kcm"
+)
+
+var (
+	fp               = tester.InitArgs("KSYUNKCM_")
+	fTestCertPath    string
+	fTestKeyPath     string
+	fAccessKeyId     string
+	fSecretAccessKey string
+)
+
+func init() {
+	fp.DefineString(&fTestCertPath, "TESTCERTPATH")
+	fp.DefineString(&fTestKeyPath, "TESTKEYPATH")
+	fp.DefineString(&fAccessKeyId, "ACCESSKEYID")
+	fp.DefineString(&fSecretAccessKey, "SECRETACCESSKEY")
+}
+
+/*
+Shell command to run this test:
+
+	go test -v ./ksyun_kcm_test.go -args \
+	--KSYUNKCM_TESTCERTPATH="/path/to/your-test-cert.pem" \
+	--KSYUNKCM_TESTKEYPATH="/path/to/your-test-key.pem" \
+	--KSYUNKCM_ACCESSKEYID="your-access-key-id" \
+	--KSYUNKCM_SECRETACCESSKEY="your-secret-access-key"
+*/
+func TestProvider(t *testing.T) {
+	fp.Parse()
+
+	t.Run("Upload", func(t *testing.T) {
+		provider, err := impl.NewCertmgr(&impl.CertmgrConfig{
+			AccessKeyId:     fAccessKeyId,
+			SecretAccessKey: fSecretAccessKey,
+		})
+		require.NoError(t, err)
+
+		tester.Upload(t, provider, tester.UploadInput{CertPath: fTestCertPath, KeyPath: fTestKeyPath})
+	})
+}
